9b8df9c4cddecfa9bb2b218997d447652b34a691
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-09-23 22:42:37 +0530

    added critera selection component

---
+++ src/components/FilterTags.vue
@@ -0,0 +1,46 @@
+<template>
+  <div @click="$emit('clicked', type)" :class="state ? 'type-meta-active' : 'type-meta'">{{ type }}</div>
+</template>
+
+<script>
+export default {
+  name: "FilterTags",
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.type-meta {
+  cursor: pointer;
+  margin-right: 2em;
+  border-radius: 30px;
+  background-color: #ffffff;
+  padding: 0.5em 1.5em;
+  color: #1c1c1c;
+  transition: all 0.2s ease-in;
+}
+
+.type-meta:hover {
+  background-color: #59FF9C;
+
+}
+
+.type-meta-active {
+  cursor: pointer;
+  margin-right: 2em;
+  border-radius: 30px;
+  background-color: #59FF9C;
+  padding: 0.5em 1.5em;
+  color: #1c1c1c; 
+}
+</style>
--- src/section/Work.vue
+++ src/section/Work.vue
@@ -1,21 +1,39 @@
 <template>
-  <div>
+  <div class="work-container">
     <h1>Work</h1>
+    <ul class="criteria-list">
+      <li v-for="item in criteria" :key="item">
+        <FilterTags v-on:clicked="criteriaSeletion" :type="item" />
+      </li>
+    </ul>
     <ProjectModule class="module" />
   </div>
 </template>
 
 <script>
 import ProjectModule from "../components/ProjectModule";
+import FilterTags from "../components/FilterTags";
 export default {
   name: "Work",
   components: {
-    ProjectModule
+    ProjectModule,
+    FilterTags
+  },
+  data() {
+    return {
+      selectedType: "",
+      criteria: ["Favorites", "Programming", "UI Design", "Graphic Design"]
+    };
+  },
+  methods: {
+    criteriaSeletion(type) {
+      this.selectedType = type;
+    }
   }
 };
 </script>
 <style scoped>
-div {
+.work-container {
   display: flex;
   flex-flow: column;
   align-items: center;
@@ -24,5 +42,12 @@ div {
 
 .module {
   margin-bottom: 10em;
+}
+
+.criteria-list {
+  display: flex;
+  justify-content: space-between;
+  list-style-type: none;
+  align-self: flex-start;
 }
 </style>
